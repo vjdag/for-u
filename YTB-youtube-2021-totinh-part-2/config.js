b359787b69602054bd0da2b5708464bdd9fe8804
vjdag <96651308+vjdag@users.noreply.github.com>
2021-12-25 18:25:27 +0700

    Add files via upload

---
+++ YTB-youtube-2021-totinh-part-2/config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Bạn cho tôi hỏi ý kiến xíu...`,
+    btnIntro: 'OK',
+    title: 'Hôm nào bạn đi chill với tôi không ? 😎',
+    desc: 'Bạn không trả lời hoặc thoát ra, tức là bạn đồng ý đó nha (◍•ᴗ•◍) ',
+    btnYes: 'For sure! 💖💖💖',
+    btnNo: '💔 Thế bạn nghĩ xem tại sao mình lại phải trả lời bạn? 💔 ',
+    question:'Cho bạn chọn lịch đấy ☞￣ᴥ￣☞ ',
+    reasonPlaceholder: 'Viết thời gian tại đây',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'i trước Tết được k 👯‍♀️ ',
+    mess: '☂️🐓 bạn mời tôi cũng k dám chối ạ',
+    messDesc: '🎁 Còn giờ thì mở gói quà hôm nọ tôi tặng đi thì mới thực hiện hóa được chứ.🎁',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.messenger.com/t/100053439350001'
+}
